b1c237f2d924051651bbc1adea8f68c25afe7f0f
Rasujon3 <rasujon3@gmail.com>
2022-02-24 18:01:48 +0600

    card done

--- cartsapi/js/index.js
+++ cartsapi/js/index.js
@@ -5,31 +5,49 @@
     error handling for empty value
 
 */
-
+const main = document.getElementById("main");
 const searchButton = () => {
   const input = document.getElementById("input-value");
   const error = document.getElementById("error");
 
-  const inputValue = input.value;
-  console.log(inputValue);
+  const inputValue = parseInt(input.value);
   if (isNaN(inputValue) || inputValue == "") {
-    // isNaN check nuber or string or others-true
+    // isNaN check number or string or others-true
     // alert("Please enter a number");
     error.innerText = "Please enter a number";
+    input.value = "";
+    main.innerHTML = "";
   } else if (inputValue <= 0) {
     error.innerText = "Please enter a positive number";
+    input.value = "";
+    main.innerHTML = "";
   } else {
-    error.style.display = "none";
+    // main.innerHTML = "";
+    main.textContent = "";
     fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
       .then((res) => res.json())
-      .then((data) => console.log(data.cards));
-  }
+      .then((data) => cardsDisplay(data.cards));
 
-  input.value = "";
+    input.value = "";
+    error.innerHTML = "";
+  }
 };
 
 const cardsDisplay = (cards) => {
   for (const card of cards) {
-    console.log(card);
+    const div = document.createElement("div");
+    div.classList.add("col-lg-4");
+    div.classList.add("mb-5");
+    div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${card.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${card.suit}</h5>
+                <p class="card-text">${card.code}</p>
+                <button href="#" class="btn btn-primary">See Datails</button>
+            </div>
+        </div>
+    `;
+    main.appendChild(div);
   }
 };
